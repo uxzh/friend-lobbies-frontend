44ed8e60fe3f1320a6336e0586502296dbabe68a
uxzh <77202660+uxzh@users.noreply.github.com>
2023-06-12 17:30:51 +0300

    interest-selection styling (#10)
    Added
    - card styling
    - card-picking functionality
    - "continue" button

--- src/components/InterestSelection/InterestButton.js
+++ src/components/InterestSelection/InterestButton.js
@@ -1,23 +1,53 @@
 import { Card, Row, Text } from "@nextui-org/react";
+import { useState } from "react";
 
+function InterestButton({ props, onInterestSelect }) {
+  const { interest, emoji, bgcolor } = props;
+  const [bgColorState, setBgColorState] = useState("");
+  const [isSelected, setIsSelected] = useState(false);
 
-function InterestButton(props){
-    const {interest} = props.props
-    return(
-        <Card 
-            variant="flat"
-            isPressable
-            // css={{width: 78, height: 78}}
-        >
-            <Card.Body>
-                <Row justify="center" align="center">
-                    <Text h6>
-                            {interest}
-                    </Text>
-                </Row>
-            </Card.Body>
-        </Card>
-    )
+  const handleClick = () => {
+    setIsSelected(!isSelected);
+    if (!isSelected) {
+      onInterestSelect(interest, true);
+    } else {
+      onInterestSelect(interest, false);
+    }
+  };
+
+  return (
+    <Card
+      variant="flat"
+      isPressable
+      css={{
+        width: "26vw",
+        height: "12vh",
+        margin: "auto",
+        minHeight: 100,
+        minWidth: 100,
+        backgroundColor: isSelected ? bgcolor : "",
+      }}
+      onClick={handleClick}
+    >
+      <Card.Body>
+        <Row justify="center" align="center">
+          <Text
+            h4
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "center",
+              marginTop: "1vh",
+            }}
+          >
+            {interest}
+            <br />
+            {emoji}
+          </Text>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default InterestButton;
--- src/pages/InterestSelection.js
+++ src/pages/InterestSelection.js
@@ -1,41 +1,77 @@
-import { Card, Container, Grid, Row, Text } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  Container,
+  Grid,
+  Row,
+  Spacer,
+  Text,
+} from "@nextui-org/react";
 import TopNavbar from "../components/navbar/TopNavbar";
 import "../styles/Main.css";
 import InterestButton from "../components/InterestSelection/InterestButton";
 
-import interests from '../data/interest_selection.json'
+import interests from "../data/interest_selection.json";
+import { useState } from "react";
+import { ArrowRightSquare } from "react-iconly";
 
+function InterestSelection() {
+  const [selectedInterests, setSelectedInterests] = useState([]);
 
-function InterestSelection(){
-    
-    return(
-        <>
-            <header>
-                <TopNavbar />
-            </header>
-            <main>
-                <Container sm>
-                    <Card>
-                        <Row justify="center">
-                            <Text h4 size="$2xl" style={{ marginTop: 8}}>
-                                Your Preferences
-                            </Text>
-                        </Row>
-                        <Grid.Container justify="center" gap={1} wrap="wrap">
-                            {
-                                interests.map((item, index)=>{
-                                    return (
-                                    <Grid xs alignItems="center" alignContent="center">
-                                        <InterestButton key={index} props={item}/>
-                                    </Grid>
-                                    )})
-                            }
-                        </Grid.Container>
-                    </Card>
-                </Container>
-            </main>
-        </>
-    )
+  const handleInterestSelection = (interest, isSelected) => {
+    if (isSelected) {
+      setSelectedInterests([...selectedInterests, interest]);
+    } else {
+      setSelectedInterests(
+        selectedInterests.filter((item) => item !== interest)
+      );
+    }
+  };
+
+  return (
+    <>
+      <header>
+        <TopNavbar />
+      </header>
+      <main>
+        <Container sm>
+          <Card style={{ marginBottom: "12vh" }}>
+            <Row justify="center">
+              <Text h3 style={{ margin: "2vh 0" }}>
+                Select your Interests
+              </Text>
+            </Row>
+            <Grid.Container justify="center" gap={1} wrap="wrap">
+              {interests.map((item, index) => {
+                return (
+                  <Grid
+                    xs
+                    alignItems="center"
+                    alignContent="center"
+                    key={index}
+                  >
+                    <InterestButton
+                      props={item}
+                      onInterestSelect={handleInterestSelection}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid.Container>
+            <Spacer />
+          </Card>
+          <Button
+            size={"lg"}
+            auto
+            iconRight={<ArrowRightSquare set="bold" />}
+            style={{ margin: "auto" }}
+          >
+            Continue
+          </Button>
+        </Container>
+      </main>
+    </>
+  );
 }
 
 export default InterestSelection;
